6882215cc9aed54dc038626dc7f70e69d7e59ec8
Brian <wamwangimathenge@gmail.com>
2022-12-03 18:45:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+// Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist
+main.remove();
+
+// has a 'newHeader' variable that points to node 'h1#victory'"
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Brian, is the champion!"
